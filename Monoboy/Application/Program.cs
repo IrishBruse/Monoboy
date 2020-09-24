90ec09a6684b898091a73bc70939d739978f1917
IrishBruse <Econn50@outlook.com>
2020-09-24 21:45:50 +0100

    Clean up

--- Monoboy/Application/Program.cs
+++ Monoboy/Application/Program.cs
@@ -23,24 +23,6 @@ namespace Monoboy.Application
             {
                 Directory.CreateDirectory("Saves");
             }
-            if(File.Exists("Config.ini") == false)
-            {
-                using StreamWriter stream = File.CreateText("Config.ini");
-                stream.WriteLine("# WIP ");
-                stream.WriteLine("# Keybinds");
-                stream.WriteLine("A:      Space");
-                stream.WriteLine("B:      ShiftLeft");
-                stream.WriteLine("Select: Escape");
-                stream.WriteLine("Start:  Enter");
-                stream.WriteLine("Up:     W");
-                stream.WriteLine("Down:   A");
-                stream.WriteLine("Left:   S");
-                stream.WriteLine("Right:  D");
-                stream.WriteLine("# Gameboy PalletSwaps");
-                stream.WriteLine("0xD0D058 0xA0A840 0x708028 0x405010 : Default");
-                stream.WriteLine("0xD0D058 0xA0A840 0x708028 0x405010 : Test");
-                stream.Close();
-            }
 
             NativeWindowSettings settings = new NativeWindowSettings
             {

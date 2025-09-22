0ff5a3aebfa837b11a5ced2ced2ac6340feaefb6
Ethan Conneely <econn50@outlook.com>
2025-09-22 21:05:53 +0100

    Add disassembler

---
+++ Monoboy.Disassembler/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Monoboy;
+
+using System.IO;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        byte[] data = File.ReadAllBytes("../roms/Tetris.gb");
+
+        foreach (var b in data)
+        {
+            Console.WriteLine(b);
+        }
+    }
+}
